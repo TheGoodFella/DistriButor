4158cc3396398341ae99327bd50dcf34c5f20af6
TheGoodFella <galas.daniele@gmail.com>
2016-05-10 18:10:50 +0200

    add magazine price and percent earn to the news stands to each magazine relase,move address field to news stand and workers, now locations stores the city without address to avoid redundant values, replaced the two queries with one procedure with task type parameter, update png

--- database/db.sql
+++ database/db.sql
@@ -12,7 +12,6 @@ CREATE TABLE locations
 	province VARCHAR(50) NOT NULL,
 	city VARCHAR(50) NOT NULL,
 	zipCode VARCHAR(50) NOT NULL,
-	address VARCHAR(50) NOT NULL,
 	PRIMARY KEY(idLocation)
 );
 
@@ -30,6 +29,7 @@ CREATE TABLE workers
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(50),
 	dateOfBirth DATE NOT NULL,
+	address VARCHAR(50) NOT NULL,
 	idLocation INTEGER NOT NULL,
 	idPhone INTEGER NOT NULL,
 	PRIMARY KEY(idWorker),
@@ -53,6 +53,8 @@ CREATE TABLE magRelases
 	idMagazine INTEGER NOT NULL,
 	magNumber INTEGER NOT NULL,
 	dateRelase DATE NOT NULL,
+  priceToPublic NUMERIC(5,2) NOT NULL,
+  percentToNS INTEGER NOT NULL,
 	PRIMARY KEY(idMagRelase, magNumber),
 	FOREIGN KEY(idMagazine) REFERENCES magazines(idMag)
 );
@@ -62,6 +64,7 @@ CREATE TABLE newsStands
 	idNewsStand INTEGER NOT NULL AUTO_INCREMENT,
 	businessName VARCHAR(50) NOT NULL,
 	piva VARCHAR(50) NOT NULL,
+	address VARCHAR(50) NOT NULL,
 	idLocation INTEGER NOT NULL,
 	idPhone INTEGER NOT NULL,
 	idOwner INTEGER NOT NULL,
@@ -102,17 +105,17 @@ CREATE TABLE tasks
 
 /*INSERT*/
 
-INSERT INTO locations VALUES (1,"Italy","Trentino","Trento","Arco","38062","Via Mantova 1"),(2,"Italy","Trentino","Trento","Arco","38062","Via Marconi 2"),(3,"Germany","Geneva","Geneva","Vernier","1214","Chemin De-Sales 3"),(4,"Italy","Trentino","Trento","Arco","38062","Via Segantini 1"),(5,"Italy","Trentino","Trento","Arco","38062","Via Castello 1"),(6,"Italy","Trentino","Trento","Arco","38062","Via Chiesa 2");
+INSERT INTO locations VALUES (1,"Italy","Trentino","Trento","Arco","38062"),(2,"Germany","Geneva","Geneva","Vernier","1214");
 
 INSERT INTO phoneNumbers VALUES (1,"0464-510001"),(2,"0464-510002"),(3,"004122-123652"),(4,"0464-511919"),(5,"0464-510003"),(6,"0464-510004"),(7,"0464-510005");
 
-INSERT INTO workers VALUES (1,"white","jack","whitej@domain.com","1980-02-01",3,3),(2,"rossi","mario","marior@domain.com","1979-03-10",1,2),(3,"bianchi","luca","bianchil@domain.com","1979-10-11",2,1),(4,"verdi","fabio","fabiov@domain.com","1962-11-16",4,4),(5,"rossini","daniele","danieler@domain.com","1992-10-05",6,7);
+INSERT INTO workers VALUES (1,"white","jack","whitej@domain.com","1980-02-01","Chemin De-Sales 3",2,3),(2,"rossi","mario","marior@domain.com","1979-03-10","Via Castello 1",1,2),(3,"bianchi","luca","bianchil@domain.com","1979-10-11","via chiesa 2",1,1),(4,"verdi","fabio","fabiov@domain.com","1962-11-16","Via Segantini 1",1,4),(5,"rossini","daniele","danieler@domain.com","1992-10-05","via italia5",1,7);
 
 INSERT INTO magazines VALUES (1,"La Busa","monthly",4),(2,"La Befusa","monthly",1);
 
-INSERT INTO magRelases VALUES (1,1,53,"2016-04-01"),(2,1,54,"2016-05-01");
+INSERT INTO magRelases VALUES (1,1,53,"2016-04-01",2,50),(2,1,54,"2016-05-01",2,50);
 
-INSERT INTO newsStands VALUES (1,"tabacchino arco","piva000001",1,5,2),(2,"tabacchino castello","piva000002",2,6,3);
+INSERT INTO newsStands VALUES (1,"tabacchino arco","piva000001","Via Mantova 1",1,5,2),(2,"news stand genevas","piva000002","Chemin De-Sales 3",2,6,3);
 
 INSERT INTO jobs VALUES (1,"Consegna numero aprile","2016-04-02"),(2,"Consegna numero maggio","2016-05-01");
 
@@ -122,13 +125,17 @@ INSERT INTO tasks VALUES (1,"deliver may copies",35,"deliver",2,1,5,2),(2,"deliv
 
 
 /*QUERIES*/
-/*show tasks delivering mags*/
-select workers.lastname, workers.name, newsStands.businessName, locations.address AS newsStands_Address, tasks.nCopies, tasks.typeTask, jobs.idJob, jobs.jobName, magRelases.magNumber AS mag_number FROM tasks JOIN workers ON tasks.idWorker=workers.idWorker JOIN newsStands ON
-tasks.idNewsStand=newsStands.idNewsStand JOIN locations ON locations.idLocation=newsStands.idLocation JOIN jobs ON jobs.idJob=tasks.idJob JOIN magRelases ON magRelases.idMagRelase=tasks.idMagRelase where tasks.typeTask="deliver";
 
-/*show tasks returning mag*/
-select workers.lastname, workers.name, newsStands.businessName, locations.address AS newsStands_Address, tasks.nCopies, tasks.typeTask, jobs.idJob, jobs.jobName, magRelases.magNumber AS mag_number FROM tasks JOIN workers ON tasks.idWorker=workers.idWorker JOIN newsStands ON
-tasks.idNewsStand=newsStands.idNewsStand JOIN locations ON locations.idLocation=newsStands.idLocation JOIN jobs ON jobs.idJob=tasks.idJob JOIN magRelases ON magRelases.idMagRelase=tasks.idMagRelase where tasks.typeTask="returner";
 /*END QUERIES*/
 
 
+/*PROCEDURES*/
+DELIMITER $$
+CREATE PROCEDURE showtask(_taskType VARCHAR(50))
+BEGIN
+	SELECT workers.lastname, workers.name, newsStands.businessName,locations.city, newsStands.address, tasks.nCopies, tasks.typeTask, jobs.idJob, jobs.jobName, magRelases.magNumber AS mag_number FROM tasks JOIN workers ON tasks.idWorker=workers.idWorker JOIN newsStands ON
+tasks.idNewsStand=newsStands.idNewsStand JOIN locations ON locations.idLocation=newsStands.idLocation JOIN jobs ON jobs.idJob=tasks.idJob JOIN magRelases ON magRelases.idMagRelase=tasks.idMagRelase where tasks.typeTask=_taskType;
+
+END $$
+DELIMITER ;
+/*END FUNCTIONS*/
